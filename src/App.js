8b6c7082b043e8f5464704d5bceffc655e32694a
andriusmw <andriusmw@yahoo.es>
2025-01-08 07:24:17 +0100

    routing wortking with components

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import { HomePage } from "./pages/HomePage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RecordPage } from "./pages/RecordPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
     <main>
       <Header />
        <Routes>
-          <Route path="/" element={HomePage} />
-          <Route path="/register" element={RegisterPage} /> 
-          <Route path="/login" element={LoginPage} />
-          <Route path="/record/:id" element={RecordPage} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage/>} /> 
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/record/:id" element={<RecordPage/>} />
+          <Route path="*" element={<NotFoundPage />} />
        </Routes>
       <Footer />
     </main>
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,8 @@
+export const NotFoundPage = () => {
+    return (
+        <section>
+            <h1>404 ERROR </h1>
+            <p>Page Not Found</p>
+        </section>
+    )
+}
