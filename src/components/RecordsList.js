4969a25c0df87986f586e3b3e0070d61e9e2934a
andriusmw <andriusmw@yahoo.es>
2025-01-09 09:57:52 +0100

    GET latest records with table

--- src/components/RecordsList.js
+++ src/components/RecordsList.js
@@ -1,15 +1,42 @@
 import { Record } from "./Record";
 
-export const RecordList = ({records}) => {
-    return records.length ? (
-    <ul>
-      {records.map(record => (
-        <li key={record.id}>
-            <Record record={record}/>
-            </li>
-            ))}
-    </ul>
-    ): (
-        <p>There are no records</p>
-    ) ;
+export const RecordList = ({ records }) => {
+  return records.length ? (
+    <table>
+      <thead>
+        <tr>
+          <th>Character Name</th>
+          <th>Class</th>
+          <th>Dungeon Name</th>
+          <th>Difficulty</th>
+          <th>Season</th>
+          <th>time</th>
+          <th>Num.Players</th>
+          <th>Server</th>
+          <th>Date</th>
+          <th>status</th>
+     
+        </tr>
+      </thead>
+      <tbody>
+        {records.map((record) => (
+          <tr key={record.id}>
+            <td>{record.character_name}</td>
+            <td>{record.class1}</td>
+            <td>{record.dungeon_name}</td>
+            <td>{record.dungeon_difficulty}</td>
+            <td>{record.dungeon_season}</td>
+            <td>{record.time}</td>
+            <td>{record.num_players}</td>
+            <td>{record.server}</td>
+            <td>{new Date(record.Date_completed).toLocaleString()}</td>
+            <td>{record.status}</td>
+           
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : (
+    <p>There are no records</p>
+  );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ export const HomePage = () => {
   
   return <section>
         <h2>Latest records!</h2>
-        <p>here we will have the list of latest records</p>
+     
         <RecordList records={records} />
     </section> 
 } 
