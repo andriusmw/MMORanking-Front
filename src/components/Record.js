a11c4f013068251f8bd310aea0b51e03980e8ce3
andriusmw <andriusmw@yahoo.es>
2025-01-10 09:33:30 +0100

    single record page working more info

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,7 +1,9 @@
 export const Record = ({record}) => {
     return (
         <article>
-      
+            
+            <button>Go back</button>
+            
             <p>{record.character_name}</p>
             <p>{record.class1}</p>
             <p>{record.server}</p>
@@ -9,9 +11,14 @@ export const Record = ({record}) => {
             <p>{record.dungeon_difficulty}</p>
             <p>{record.dungeon_season}</p>
             <p>{record.time}</p>
+            <p>{record.date_completed}</p>
             <p>{record.num_players}</p>
             <p>{record.log_link}</p>
+            <p>{record.video_link}</p>
             <p>{record.status}</p>
+
+            <button>Aprove</button>
+            <button>Delete</button>
         </article>
     ) 
 }
