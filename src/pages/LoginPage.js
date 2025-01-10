5e5af037883cbe0a9fe2f4acd203c45b92ec1f17
andriusmw <andriusmw@yahoo.es>
2025-01-10 14:54:10 +0100

    loginpage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,41 @@
+import { useState } from "react";
+
 export const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassowrd] = useState("");
+    const [error, setError] = useState("");
+
+    const handleForm = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+
+        } catch(error) {
+
+        }
+    }
+
     return (
         <section>
             <h1>Login</h1>
-            <p>Here we will have our Login form</p>
+           <form onSubmit={handleForm}>
+            <fieldset>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" required 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" required
+                     onChange={(e) => setPassowrd(e.target.value)}
+                />
+            </fieldset>
+
+            <button>LogIn</button>
+            {error ? <p>{error}</p>: null}
+           </form>
         </section>
     )
 }
