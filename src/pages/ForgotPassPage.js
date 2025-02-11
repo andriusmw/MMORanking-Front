bd1fae247c12bbbb3595168735030e4828a506e0
andriusmw <andriusmw@yahoo.es>
2025-02-11 12:55:40 +0100

    sending code on form1, checking on form 2 and showing form 3 calling function after passwords are the same

--- src/pages/ForgotPassPage.js
+++ src/pages/ForgotPassPage.js
@@ -8,6 +8,11 @@ export const ForgotPassPage = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const [reccode, setReccode] = useState("");
+    const [reccodeForm, setReccodeForm] = useState("");
+    const [visible2, setVisible2] = useState(false);
+    const [sending, setSending] = useState(false);
+    const [pass1, setPass1] = useState("");
+    const [pass2, setPass2] = useState("");
 
     const {login} = useContext(AuthContext)
     const navigate = useNavigate();
@@ -22,11 +27,11 @@ export const ForgotPassPage = () => {
             // and receive it. store it in a state.to send it with the email to the next component. 
             const data = await getUserByEmailService(email);
            
-            console.log(data);
+           // console.log(data);
            // console.log(data.reccode);
             //guardando el recoverycode en el estado
-           // setReccode(data.reccode);
-      
+            setReccode(data.reccode);
+            setVisible2(true);
         
        
 
@@ -36,9 +41,66 @@ export const ForgotPassPage = () => {
         }
     }
 
-    return (
-        <section>
-            <h1>FORGOT YOUR PASSWORD?</h1>
+
+
+        // HANDLE FORM PHASE 2--------------------------
+
+        const handleForm2 = async (e) => {
+            e.preventDefault();
+            setError("");
+    
+            try {
+                //check if the recovery code on the form is the same as in the user.reccode
+                if (reccode == reccodeForm) {
+                    console.log("pasa a phase 3")
+                } else {
+                    setError(error.message);
+                }
+
+            } catch(error) {
+                setError(error.message);
+    
+            }
+        }
+
+
+        // HANDLE FORM PHASE 3--------------------------
+
+        const handleForm3 = async (e) => {
+            e.preventDefault();
+            setError("");
+    
+            try {
+                //check if the recovery code on the form is the same as in the user.reccode
+                if (pass1 == pass2) {
+                    console.log("llama al service")
+                } else {
+                    setError("passwords are not the same");
+                }
+
+            } catch(error) {
+                setError(error.message);
+    
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    return ( 
+    <>
+        {/* FORM 1 FORGOT YOUR PASSWORD? */}
+        {visible2 ? (null) : (
+            <section>
+            <h1>FORGOT YOUR PASSWORD? PHASE 1</h1>
             <p>If you forgot your password or want to change it, please write down your email and you will receive
                 a code for changing your password. 
             </p>
@@ -55,5 +117,57 @@ export const ForgotPassPage = () => {
             {error ? <p>{error}</p>: null}
            </form>
         </section>
+        )}
+        
+
+         {/* FORM 2 FORGOT YOUR PASSWORD? */}
+        { visible2 ? (
+         <section>
+            <h1>FORGOT YOUR PASSWORD? PHASE 2</h1>
+            <p>Introduce the code you received by E-mail. 
+            </p>
+           <form onSubmit={handleForm2}>
+            <fieldset>
+                <label htmlFor="recCodeForm">Email</label>
+                <input type="text" id="recCodeForm" name="recCodeForm" required 
+                    onChange={(e) => setReccodeForm(e.target.value)}
+                />
+            </fieldset>
+           
+
+            <button>Check Code</button>
+            {error ? <p>{error}</p>: null}
+           </form>
+        </section> ) : (null) }
+
+              {/* FORM 3 FORGOT YOUR PASSWORD? */}
+        { visible2 ? (
+         <section>
+            <h1>FORGOT YOUR PASSWORD? PHASE 3</h1>
+            <p>Introduce the the new password 
+            </p>
+           <form onSubmit={handleForm3}>
+            <fieldset>
+                <label htmlFor="pass1">Password:</label>
+                <input type="password" id="pass1" name="pass1" required 
+                    onChange={(e) => setPass1(e.target.value)}
+                />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="pass2">Repeat password:</label>
+                <input type="password" id="pass2" name="pass2" required 
+                    onChange={(e) => setPass2(e.target.value)}
+                />
+            </fieldset>
+           
+
+            <button>Check Code</button>
+            {error ? <p>{error}</p>: null}
+           </form>
+        </section> ) : (null) }
+
+
+
+    </>
     )
 }
