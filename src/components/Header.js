81f6e7feb71bafa9858bbeb42698494d350174c7
andriusmw <andriusmw@yahoo.es>
2025-02-20 15:05:24 +0100

    NavBar created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 import { Auth } from "./Auth";
-
-
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 export const Header = () => {
-  
+    const {token, user, logout} = useContext(AuthContext);
 
     return (
         <header>
             <h1> <Link to={'/'}>Speed Run Dungeons</Link> </h1>
             <nav>
                 <Auth />
+                <navBar>
+                  <span><Link to={'/'}>Home</Link></span> 
+                  <span><Link to={'/news'}>News</Link></span> 
+                  <span><Link to={'/ladders'}>Ladders</Link></span>
+                  <span><Link to={'/newrecord'}>New Record</Link></span>
+                  <span><Link to={'/profile'}>Profile</Link></span>
+                  <span><Link to={'/stats'}>Stats</Link></span>
+                  <span><Link to={'/contact'}>Contact</Link></span>
+                
+               {  user?.user?.role == "admin" ? <span><Link to={'/admin-panel'}>Admin Panel</Link></span> : null}
+                </navBar>
             </nav>
         </header>
     );
