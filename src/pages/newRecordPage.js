fcef33240b74385ffa03fa42ab07e14ff1c4e790
Andriusmw <andriusmw@yahoo.es>
2025-03-06 16:04:36 +0100

    instructions on newrecordpage responsive

--- src/pages/newRecordPage.js
+++ src/pages/newRecordPage.js
@@ -132,11 +132,13 @@ export const NewRecordPage = () => {
       {/* Paso 1 */}
       <h4>STEP 1: TRANSLATE YOUR LOG TO ENGLISH</h4>
       <ul>
-        <li>
+        <li><p className="p-record-instructions">
           (In case your log is in a different language) Its only 2 clicks.
+          </p>
         </li>
-        <li>
+        <li><p>
           -1.1 Open your log, no matter the place inside the log you are in, just go to the right corner and select "language" then "English".
+          </p>
           <br />
           <img
             src={`${process.env.REACT_APP_BACKEND}/images/English.png`}
@@ -145,7 +147,9 @@ export const NewRecordPage = () => {
           />
         </li>
         <li>
+        <p className="p-record-instructions">
           -1.2 When you are going to copy the "correct" URL on "STEP 2A/B" make sure to click on "translate" if the log doesn't appear on English.
+          </p>
           <br />
           <img
             src={`${process.env.REACT_APP_BACKEND}/images/English-2.png`}
@@ -157,12 +161,14 @@ export const NewRecordPage = () => {
 
       {/* Paso 2A */}
       <h4>STEP 2A: If you are going to upload a log from a Dungeon in Mythic+2,3,4...up to +10</h4>
-      <p>
+      <p className="p-record-instructions">
         If you are going to upload a log from a Dungeon in Normal, Heroic or Mythic +0 mode, go down to "STEP 2B", if you are going to upload a log from Dungeon in Mythic+2,3,4...up to +10 stay here.
       </p>
       <ul>
         <li>
+          <p className="p-record-instructions">
           -2A.1 - Upload your log to Warcraft log's website as usual and then go to view yor log, select the Dungeon you want to upload the data here.
+         </p>
           <br />
           <img
             src={`${process.env.REACT_APP_BACKEND}/images/upload-log-1.png`}
@@ -171,7 +177,9 @@ export const NewRecordPage = () => {
           />
         </li>
         <li>
+        <p className="p-record-instructions">
           -2A.2 - Once the details view of the selected dungeon is loaded, copy the URL as shown on the image. Remember to translate to english first if you need it as shown on STEP 1.
+         </p>
           <br />
           <img
             src={`${process.env.REACT_APP_BACKEND}/images/upload-log-2.png`}
@@ -183,12 +191,14 @@ export const NewRecordPage = () => {
 
       {/* Paso 2B */}
       <h4>STEP 2B: This is to upload a log on Normal, Heroic or Mythic+0 mode</h4>
-      <p>
+      <p className="p-record-instructions">
         This is to upload a log on Normal, Heroic or Mythic+0 mode, intended to 1 Player or 2 Players for example. (Everyone can play this mode)
       </p>
       <ul>
-        <li>
+        <li> 
+        <p className="p-record-instructions">
           -2B.1- When you try to do a Dungeon in Normal, Heroic or Mythic+0 mode, the time counts each time, you hit an enemy, so we recomend login in front of the door, use consumeables and once you are ready go inside, clear the dungeon, use a stone to change your zone and end the record. Then you upload it before hitting anyone or anything. Otherwise the data may be corrupted and the log useless.
+        </p>
           <br />
           So once you clear the dungeon, you upload the log and should see something like this:
           <br />
@@ -199,7 +209,9 @@ export const NewRecordPage = () => {
           />
         </li>
         <li>
+        <p className="p-record-instructions">
           -2B.2 - Then you click on "Encounters and trash", remember to translate the log if it isn't in English, and copy the URL.
+         </p>
           <br />
           <img
             src={`${process.env.REACT_APP_BACKEND}/images/normal-2.png`}
